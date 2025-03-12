d5402d0a89691a3bc22ebf90534a70fe6a31ddf8
tiffhn <156131887+tiffhn@users.noreply.github.com>
2025-03-12 14:18:46 -0400

    adding route to sign up

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import AccountSettings from "./components/AccountSettings";
 import ChangePassword from "./components/ChangePassword";
 import Location from "./components/Location";
 import CameraPermissions from "./components/CameraPermissions";
+import SignUp from './components/SignUp';
 
 const App = () => {
   // Load dark mode preference from localStorage
@@ -104,6 +105,7 @@ const AppContent = ({ darkMode, setDarkMode, locationEnabled, setLocationEnabled
       >
         <Routes>
           <Route path="/" element={<PWAWelcomeLogin />} />
+          <Route path="/signup" element={<SignUp />} />
 
           {/* Desktop Routes */}
           {!isMobile && (
